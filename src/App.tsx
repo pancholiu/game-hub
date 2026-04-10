6f40c928132bdfa6d2330b0536289be953e4f95a
Francisco Liu <pancholiu@hotmail.com>
2026-04-10 18:33:17 -0400

    Display genres pictures

--- src/App.tsx
+++ src/App.tsx
@@ -12,12 +12,16 @@ function App() {
         base: `"nav" "main"`,
         lg: `"nav nav" "aside main"`,
       }}
+      templateColumns={{
+        base: "1fr",
+        lg: "200px ",
+      }}
     >
       <GridItem area="nav">
         <NavBar />
       </GridItem>
       {showAside && (
-        <GridItem area="aside">
+        <GridItem area="aside" paddingX={5}>
           <GenreList />
         </GridItem>
       )}
